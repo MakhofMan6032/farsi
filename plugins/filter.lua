1ce458af81a5c25102e6054c262027df85e63615
MakhofMan6032 <MakhofMan2000@gmail.com>
2016-08-25 23:40:36 +0430

    Update filter.lua

--- plugins/filter.lua
+++ plugins/filter.lua
@@ -71,7 +71,7 @@ function clear_commandsbad(msg, cmd_name)
 end
 
 local function run(msg, matches)
-  if matches[2] == 'filter' then
+  if matches[2] == 'فیلتر' then
   if not is_momod(msg) then
    return 'only for moderators'
   end
@@ -80,13 +80,13 @@ local function run(msg, matches)
   local text = addword(msg, name)
   return text
   end
-  if matches[2] == 'filterlist' then
+  if matches[2] == 'لیست فیلتر' then
   return list_variablesbad(msg)
   elseif matches[2] == 'clean' then
 if not is_momod(msg) then return '_|_' end
   local asd = '1'
     return clear_commandbad(msg, asd)
-  elseif matches[2] == 'unfilter' or matches[2] == 'rw' then
+  elseif matches[2] == 'باز کردن' or matches[2] == 'باز کردن' then
    if not is_momod(msg) then return '_|_' end
     return clear_commandsbad(msg, matches[3])
   else
@@ -98,11 +98,11 @@ end
 
 return {
   patterns = {
-  "^([!/#])(rw) (.*)$",
-  "^([!/#])(filter) (.*)$",
-   "^([!/#])(unfilter) (.*)$",
-    "^([!/#])(filterlist)$",
-    "^([!/#])(clean) filterlist$",
+  "^(باز کردن) (.*)$",
+  "^(فیلتر) (.*)$",
+   "^(باز کردن) (.*)$",
+    "^(لیست فیلتر)$",
+    "^(پاک کردن)$",
 "^(.+)$",
 	   
   },
